d3d13a756d7c66b8c07937923b039a0ace560fdf
SaeeSaadat <saee_saadat@outlook.com>
2021-07-15 19:27:45 +0430

    view

--- SQL/create_views.sql
+++ SQL/create_views.sql
@@ -5,5 +5,25 @@ from (
         join user_person up on p.id = up.person_id
          ) as pp
 where (pp.username = (select user)) ;
+
 grant SELECT on people to simpleton;
 
+
+select * from people;
+
+select * from person;
+
+
+create view my_votes as
+select *
+from ( select v.*, up.username from
+     vote as v
+     join user_person as up on v.person_id = up.person_id
+         ) as t
+where t.username = (select user) ;
+
+select * from my_votes;
+
+drop view my_votes;
+
+grant select on my_votes to simpleton;
